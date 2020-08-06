3f0833dbd7afc4ab46ea17698741978942482a4d
Difinition <vladdifinition@mail.ru>
2020-08-07 01:12:27 +0300

    Рефакторинг

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {Router, Route, Switch} from 'react-router-dom';
 import Main from '../main/Main.jsx';
 import MoviePage from '../movie-detail/Movie.jsx';
@@ -10,51 +10,45 @@ import MyList from '../my-list/my-list.jsx';
 import PrivateRoute from '../private-route/private-route.jsx';
 import {AuthorizationStatus} from '../../reducer/user/user';
 
-export class App extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Router history={history}>
-        <Switch>
-          <Route exact path="/films/:id" component={MoviePage} />
-          <Route exact path="/films/:id/player" component={FullScreenPlayer} />
-          <PrivateRoute
-            exact
-            path='/films/:id/review'
-            requiredAuthStatus={AuthorizationStatus.AUTH}
-            pathToRedirect={`/login`}
-            render={(match) => {
-              return <Review
-                match={match}
-              />;
-            }}
-          />
-          <PrivateRoute
-            exact
-            path='/mylist'
-            requiredAuthStatus={AuthorizationStatus.AUTH}
-            pathToRedirect={`/login`}
-            render={() => {
-              return <MyList/>;
-            }}
-          />
-          <PrivateRoute
-            exact
-            path="/login"
-            requiredAuthStatus={AuthorizationStatus.NO_AUTH}
-            pathToRedirect={`/`}
-            render={() => {
-              return <SignIn />;
-            }}
-          />
-          <Route exact path='/' component={Main} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/films/:id" component={MoviePage} />
+        <Route exact path="/films/:id/player" component={FullScreenPlayer} />
+        <PrivateRoute
+          exact
+          path='/films/:id/review'
+          requiredAuthStatus={AuthorizationStatus.AUTH}
+          pathToRedirect={`/login`}
+          render={(match) => {
+            return <Review
+              match={match}
+            />;
+          }}
+        />
+        <PrivateRoute
+          exact
+          path='/mylist'
+          requiredAuthStatus={AuthorizationStatus.AUTH}
+          pathToRedirect={`/login`}
+          render={() => {
+            return <MyList/>;
+          }}
+        />
+        <PrivateRoute
+          exact
+          path="/login"
+          requiredAuthStatus={AuthorizationStatus.NO_AUTH}
+          pathToRedirect={`/`}
+          render={() => {
+            return <SignIn />;
+          }}
+        />
+        <Route exact path='/' component={Main} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
