19d5180492ee7c173ca8c68eb44fc3bb5fb10d05
Difinition <vladdifinition@mail.ru>
2020-11-08 23:04:40 +0300

    Рефакторинг селекторов

--- src/reducer/user/selectors.js
+++ src/reducer/user/selectors.js
@@ -2,18 +2,10 @@ import NameSpace from "../name-space.js";
 
 const NAME_SPACE = NameSpace.USER;
 
-export const getAuthorizationStatus = (state) => {
-  return state[NAME_SPACE].authorizationStatus;
-};
+export const getAuthorizationStatus = (state) => state[NAME_SPACE].authorizationStatus;
 
-export const getAuthorizationError = (state) => {
-  return state[NAME_SPACE].authorizationError;
-};
+export const getAuthorizationError = (state) => state[NAME_SPACE].authorizationError;
 
-export const getUser = (state) => {
-  return state[NAME_SPACE].user;
-};
+export const getUser = (state) => state[NAME_SPACE].user;
 
-export const getLoadingAuthStatus = (state) => {
-  return state[NAME_SPACE].isAuthorizationLoading;
-};
+export const getLoadingAuthStatus = (state) => state[NAME_SPACE].isAuthorizationLoading;
